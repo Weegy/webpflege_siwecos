54a62f0ca2c95fd433ecd2bd2be6823968d65848
Marcel Wege <mwege@byte5.de>
2019-03-08 22:59:35 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <input type="text" class="form-control" id="domain" v-model="domain" />
+    
+    <button class="btn btn-success" @click="enterDomain">Abschicken</button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
+import { SiwecosRequest } from './interface/siwecosrequest';
 
 @Component({
   components: {
-    HelloWorld,
+  },
+  data() {
+    return {
+      domain: '',
+      requestId: 0,
+      result: {}
+    };
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public enterDomain() {
+    const siweocsData: SiwecosRequest = {domain: this.$data.domain};
+    axios.post('https://ca.siwecos.de/api/v1/getFreeScanStart', siweocsData).then((result) => {
+      this.$data.requestId = result.data.id;
+      this.pollRequest();
+    });
+  }
+
+  public async pollRequest() {
+    const self = this;
+    await setTimeout(() => {
+        axios.get(`https://ca.siwecos.de/api/v1/scan/status/free/${self.$data.requestId}`).then((result) => {
+        if (result.data.progress === 100) {
+          self.processResults();
+        } else {
+          self.pollRequest();
+        }
+      });
+    }, 2000);
+  }
+
+  public processResults() {
+    axios.get(`https://bla.siwecos.de/api/v1/freescan/result/${this.$data.requestId}/de`).then((result) => {
+      this.$data.result = result.data;
+      console.log(this.$data.result); 
+    })
+  }
+}
 </script>
 
 <style lang="scss">
